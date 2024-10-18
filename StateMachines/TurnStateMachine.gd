30f17fe5d7a6dc5a9ce99a7f41243290675ac0f6
thannicko <thanh@home>
2024-10-18 18:55:39 +0200

    Show worm for dice 6

--- StateMachines/TurnStateMachine.gd
+++ StateMachines/TurnStateMachine.gd
@@ -5,12 +5,11 @@ var _nr_dices_left: int = MaxThrows
 var _dices_frequency: Dictionary = {}
 var _kept_dices: Dictionary = {}
 
-
 const _dice_textures: Dictionary = {
 	1: "res://Assets/dice-one.png",
 	2: "res://Assets/dice-two.png",
 	3: "res://Assets/dice-three.png",
 	4: "res://Assets/dice-four.png",
 	5: "res://Assets/dice-five.png",
-	6: "res://Assets/dice-six.png"
+	6: "res://Assets/worm.png"
 }
