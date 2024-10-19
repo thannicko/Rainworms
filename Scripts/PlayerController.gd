da1737db986026f2914e6f9ade5c7d1f6b11a456
thannicko <thanh@home>
2024-10-20 00:43:47 +0200

    Give a better name to my test player

--- Scripts/PlayerController.gd
+++ Scripts/PlayerController.gd
@@ -6,7 +6,7 @@ var _deck: TileDeck = load("res://Deck.tres")
 
 func _ready() -> void:
 	var test_player := Player.new()
-	test_player.name = "Chiichii"
+	test_player.name = "Thannicko"
 	test_player.points_earned_in_turn = 30
 	_players.append(test_player)
 	_active_player = test_player
